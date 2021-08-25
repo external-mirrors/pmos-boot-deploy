3e4e20590a0927f4cce76c978c712fc0b751b276
Clayton Craft <clayton@craftyguy.net>
2021-08-25 09:41:40 -0700

    test_boot_deploy_functions: improve logging output for file size test

--- test_boot_deploy_functions.sh
+++ test_boot_deploy_functions.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 # shellcheck disable=SC1091
 . boot-deploy-functions.sh
@@ -16,15 +16,17 @@ cleanup() {
 test_get_size_of_files() {
 	wdir=$(mktemp -d /tmp/boot-deploy-test.XXXXXX)
 	for f in f1 f2 f3 f4 f5; do
-		dd if=/dev/zero of="$wdir/$f" bs=1K count=37
+		dd if=/dev/zero of="$wdir/$f" bs=1K count=37 >/dev/null 2>&1
 	done
 	size=$(get_size_of_files "$wdir/f1 $wdir/f2 $wdir/f3 $wdir/f4 $wdir/f5")
 
 	# exact size depends on the filesystem, so just make sure it's roughly in the same ballpark
 	if [ "$size" -ge 225 ] || [ "$size" -le 150 ]; then
-		echo "test_get_size_of_files failed, expected: ~150-225 kilobytes, got: $size kilobytes"
+		echo "test_get_size_of_files: failed, expected: ~150-225 kilobytes, got: $size kilobytes"
 		cleanup "$wdir" 1
 	fi
+
+	echo "test_get_size_of_files: pass"
 
 	cleanup "$wdir" 0
 }
