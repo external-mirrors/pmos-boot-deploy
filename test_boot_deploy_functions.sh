9c90f342dd40779e965bfbf4e97a309658995d22
Clayton Craft <clayton@craftyguy.net>
2023-03-15 23:43:08 -0700

    copy_files: support specifying destination for a given file
    This adds support for passing a file path with the format <src>:<dest>,
    where <dest> is a path within $output_dir.
    
    For example: `grub.cfg:/grub/grub.cfg`
    Will copy `grub.cfg` to $output_dir/grub/grub.cfg

--- test_boot_deploy_functions.sh
+++ test_boot_deploy_functions.sh
@@ -10,7 +10,7 @@ test_get_size_of_files() {
 	for f in f1 f2 f3 f4 f5; do
 		dd if=/dev/zero of="$wdir/$f" bs=1K count=37 >/dev/null 2>&1
 	done
-	size=$(get_size_of_files "$wdir/f1 $wdir/f2 $wdir/f3 $wdir/f4 $wdir/f5")
+	size=$(get_size_of_files "$wdir/f1 $wdir/f2 $wdir/f3 $wdir/f4 $wdir/f5:/foo/f5")
 
 	ret=0
 	# exact size depends on the filesystem, so just make sure it's roughly in the same ballpark
@@ -21,10 +21,32 @@ test_get_size_of_files() {
 
 	[ $ret -eq 0 ] && echo "test_get_size_of_files: pass"
 	return $ret
+}
 
+test_copy_files() {
+	output_dir="$wdir/out"
+	_in="$wdir/in"
+	mkdir -p "$_in"
+	mkdir -p "$output_dir"
+	touch "$_in/foo"
+	touch "$_in/bar"
+
+	copy_files "$_in/foo:/usr/bin/foo $_in/bar"
+
+	ret=0
+	if [ ! -e "$output_dir/usr/bin/foo" ]; then
+		echo "test_copy_files: fail - expected to copy a file with src:dest format!"
+		ret=1
+	elif [ ! -e "$output_dir/bar" ]; then
+		echo "test_copy_files: fail - expected to copy a file!"
+		ret=1
+	fi
+	[ $ret -eq 0 ] && echo "test_copy_files: pass"
+	return $ret
 }
 
 test_get_size_of_files
+test_copy_files
 
 rm -rf "$wdir"
 trap - INT EXIT TERM
