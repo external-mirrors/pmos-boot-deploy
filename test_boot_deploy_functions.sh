d2137a7e9980ed8b973ae574d11e33bb3efac02a
Clayton Craft <clayton@craftyguy.net>
2023-03-19 14:32:09 -0700

    test_boot_deploy_functions: improve cleanup and status reporting
    This allows running more tests later, since using "exit" would just quit
    the script when the test is done... This also makes wdir "global" and
    traps signals for cleanup after.

--- test_boot_deploy_functions.sh
+++ test_boot_deploy_functions.sh
@@ -3,32 +3,25 @@
 # shellcheck disable=SC1091
 . boot-deploy-functions.sh
 
-# $1: directory to remove/cleanup
-# $2: return code
-cleanup() {
-	if [ -d "$1" ]; then
-		rm -r "$1"
-	fi
-
-	exit "$2"
-}
+wdir="$(mktemp -d /tmp/boot-deploy-test.XXXXXX)"
+trap 'rm -rf $wdir' INT EXIT TERM
 
 test_get_size_of_files() {
-	wdir=$(mktemp -d /tmp/boot-deploy-test.XXXXXX)
 	for f in f1 f2 f3 f4 f5; do
 		dd if=/dev/zero of="$wdir/$f" bs=1K count=37 >/dev/null 2>&1
 	done
 	size=$(get_size_of_files "$wdir/f1 $wdir/f2 $wdir/f3 $wdir/f4 $wdir/f5")
 
+	ret=0
 	# exact size depends on the filesystem, so just make sure it's roughly in the same ballpark
 	if [ "$size" -ge 225 ] || [ "$size" -le 150 ]; then
 		echo "test_get_size_of_files: failed, expected: ~150-225 kilobytes, got: $size kilobytes"
-		cleanup "$wdir" 1
+		ret=1
 	fi
 
-	echo "test_get_size_of_files: pass"
+	[ $ret -eq 0 ] && echo "test_get_size_of_files: pass"
+	return $ret
 
-	cleanup "$wdir" 0
 }
 
-test_get_size_of_files
+test_get_size_of_files || exit 1
