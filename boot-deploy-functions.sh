external-mirrors/pmos-boot-deploy

commit 783a9c40d15761472c6f364b47edc533e2357be8

get_free_space: fix space calculation (MR 28)
it used to report 11% more available space

boot-deploy-functions.sh
@@ -127,7 +127,7 @@ get_free_space() {
 	# so cut fields are consistent
 	local _df_out
 	_df_out="$(df -P "$1" | tail -1 | awk '{ print $4; }')"
-	_df_out="$(echo "$_df_out"/0.9 | bc -s)"
+	_df_out="$(echo "$_df_out*0.9" | bc -s)"
 	echo "$_df_out"
 }
 

commit 9b47d4782a603a1fdd69bf8d3ba3a2094337e03b

get_free_space: always return an integer (MR 31)
Fixes an issue introduced in 783a9c40, where get_free_space would return
a float but none of the other comparisons, etc in the app would deal
with the float appropriately. Dealing with floats for comparisons is
annoying... for example: [ $(echo "$foo > 123" | bc) -eq 1 ] && whatever

This change just floors the free space value, which means it might be
slightly off from actual (by less than a byte...), but it allows us to
avoid overly complicated code for comparing to it.

This also removed an old comment that is no longer relevant.

boot-deploy-functions.sh
@@ -117,18 +117,21 @@ get_options() {
 }
 
 # Return the free space (bytes) for the mount point that contains the given
-# path. This only returns 90% of the actual free space, to avoid possibly
-# filling the filesystem
+# path. This only returns up to 90% of the actual free space, to avoid possibly
+# filling the filesystem.
+# Note: In order to avoid returning a float, which makes comparisons and other
+# operations more complicated, this function rounds down to the nearest byte.
+#
 # $1: Path
 get_free_space() {
 	[ -z "$1" ] && log "No path given to free space check" && exit 1
 
-	# note: tr is used to reduce extra spaces in df output to a single space,
-	# so cut fields are consistent
 	local _df_out
 	_df_out="$(df -P "$1" | tail -1 | awk '{ print $4; }')"
 	_df_out="$(echo "$_df_out*0.9" | bc -s)"
-	echo "$_df_out"
+	# Effectively convert the value to an integer, rounding down to the nearest
+	# byte.
+	printf "%.0f" "$_df_out"
 }
 
 # Validate deviceinfo variables, exiting the program if any of them fail